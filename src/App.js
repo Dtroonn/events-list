46616b3c0bbca12e372af274dee20e063d9a5aa4
Dmitry Plotnikov <dtroonnfls@gmail.com>
2021-06-07 10:31:10 +0300

    ya na rebote

--- src/App.js
+++ src/App.js
@@ -1,66 +1,62 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
-import LinearProgress from "@material-ui/core/LinearProgress";
-import { makeStyles } from "@material-ui/core/styles";
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { makeStyles } from '@material-ui/core/styles';
 
-import { CreateEventPage, HomePage, Page404 } from "./pages";
-import { Header } from "./components";
+import { CreateEventPage, HomePage, Page404 } from './pages';
+import { Header } from './components';
 
-import { eventsReducer } from "./state/reducers";
+import { eventsReducer } from './state/reducers';
 
-import { setEvents } from "./state/actions/events";
+import { setEvents } from './state/actions/events';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-  },
+    root: {
+        width: '100%',
+    },
 }));
 
 function App() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const [events, dispatch] = React.useReducer(eventsReducer, []);
-  const [isLoading, setIsLoading] = React.useState(true);
+    const [events, dispatch] = React.useReducer(eventsReducer, []);
+    const [isLoading, setIsLoading] = React.useState(true);
 
-  React.useEffect(() => {
-    const events = localStorage.getItem("events");
-    if (events && events.length > 0) {
-      dispatch(setEvents(JSON.parse(events)));
+    React.useEffect(() => {
+        const events = localStorage.getItem('events');
+        if (events && events.length > 0) {
+            dispatch(setEvents(JSON.parse(events)));
+        }
+        setIsLoading(false);
+    }, []);
+
+    if (isLoading) {
+        return (
+            <div className={classes.root}>
+                <LinearProgress />
+                <div className="">hello</div>
+            </div>
+        );
     }
-    setIsLoading(false);
-  }, []);
 
-  if (isLoading) {
     return (
-      <div className={classes.root}>
-        <LinearProgress />
-      </div>
+        <Router>
+            <Header />
+            <main>
+                <Switch>
+                    <Route exact path="/">
+                        <HomePage events={events} dispatch={dispatch} />
+                    </Route>
+                    <Route exact path="/create-event/:id?">
+                        <CreateEventPage dispatch={dispatch} events={events} />
+                    </Route>
+                    <Route path="/404" component={Page404} />
+                    <Redirect to="/404" />
+                </Switch>
+            </main>
+        </Router>
     );
-  }
-
-  return (
-    <Router>
-      <Header />
-      <main>
-        <Switch>
-          <Route exact path="/">
-            <HomePage events={events} dispatch={dispatch} />
-          </Route>
-          <Route exact path="/create-event/:id?">
-            <CreateEventPage dispatch={dispatch} events={events} />
-          </Route>
-          <Route path="/404" component={Page404} />
-          <Redirect to="/404" />
-        </Switch>
-      </main>
-    </Router>
-  );
 }
 
 export default App;
